baf5d23e8a6a3742a931943bb1fea4db98677c62
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2021-03-02 18:41:42 +0000

    Initial compiled version from legacy core scripts

---
+++ src/ensembl/production/toolkit/rest_client.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# .. See the NOTICE file distributed with this work for additional information
+#    regarding copyright ownership.
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+import argparse
+import logging
+
+from ensembl.production.core.rest import RestClient
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Run Production jobs via a REST service')
+
+    parser.add_argument('-u', '--uri', help='HC REST service URI', required=True)
+    parser.add_argument('-a', '--action', help='Action to take',
+                        choices=['submit', 'retrieve', 'list', 'delete', 'collate'], required=True)
+    parser.add_argument('-i', '--job_id', help='HC job identifier to retrieve')
+    parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
+    parser.add_argument('-o', '--output_file', help='File to write output as JSON', type=argparse.FileType('w'))
+    parser.add_argument('-f', '--failure_only', help='Show failures only', action='store_true')
+    parser.add_argument('-e', '--email', help='User email')
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    else:
+        logging.basicConfig(level=logging.INFO, format='%(message)s')
+
+    if not args.uri.endswith('/'):
+        args.uri = args.uri + '/'
+
+    client = RestClient(args.uri)
+
+    if args.action == 'retrieve':
+        job = client.retrieve_job(args.job_id)
+        client.print_job(job, print_results=True, print_input=True)
+    elif args.action == 'list':
+        jobs = client.list_jobs()
+    elif args.action == 'delete':
+        client.delete_job(args.job_id)
+    else:
+        logging.error("Unknown action %s", args.action)
---
+++ src/ensembl/production/toolkit/run_metadata.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# .. See the NOTICE file distributed with this work for additional information
+#    regarding copyright ownership.
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#        http://www.apache.org/licenses/LICENSE-2.0
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+
+import argparse
+import logging
+import time
+
+from ensembl.production.core.models.hive import HiveInstance
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+parser = argparse.ArgumentParser(description='Load a database into metadata')
+parser.add_argument('--metadata_uri', metavar='m', type=str, nargs=1, required=True,
+                    help='URI of metadata database')
+parser.add_argument('--database_uri', metavar='d', type=str, nargs=1, required=True,
+                    help='URI of database to load')
+parser.add_argument('--hive_uri', metavar='H', type=str, nargs=1, required=True,
+                    help='URI of hive database')
+parser.add_argument('--e_release', metavar='l', type=int, nargs=1,
+                    help='Ensembl release number')
+parser.add_argument('--release_date', metavar='r', type=str, nargs=1,
+                    help='Release date')
+parser.add_argument('--eg_release', metavar='g', type=int, nargs=1,
+                    help='Ensembl Genomes release number')
+parser.add_argument('--current_release', metavar='s', type=int, nargs=1,
+                    help='Is this the current release')
+parser.add_argument('--sleep', metavar='s', type=int, nargs='?', default=30,
+                    help='Time to wait between polling for results')
+
+args = parser.parse_args()
+
+logging.debug(args)
+
+logging.info("Connecting to hive")
+hive = HiveInstance(args.hive_uri[0])
+
+input = {"metadata_uri": args.metadata_uri[0]}
+if args.database_uri is not None:
+    input['database_uri'] = args.database_uri[0]
+if args.e_release is not None:
+    input['e_release'] = args.e_release[0]
+if args.release_date is not None:
+    input['release_date'] = args.release_date[0]
+if args.eg_release is not None:
+    input['eg_release'] = args.eg_release[0]
+if args.current_release is not None:
+    input['current_release'] = args.current_release[0]
+
+logging.info("Submitting job with arguments " + str(input))
+job = hive.create_job('metadata_updater_processdb', input)
+
+logging.info("Job submitted with ID " + str(job.job_id))
+
+output = None
+while True:
+    logging.info("Sleeping for %ds" % args.sleep)
+    time.sleep(args.sleep)
+    output = hive.get_result_for_job_id(job.job_id)
+    if output['status'] != 'incomplete':
+        logging.debug("Job finished with status " + output['status'])
+        break
+
+if output['status'] == 'failed':
+    msg = hive.get_job_failure_msg_by_id(job.job_id)
+    logging.error("Job failed with error " + msg.msg)
+else:
+    print("Status: %s\n" % output['status'])
