a23734b6c68d0c7440029fffad95bb26737026de
James Allen <jallen@ebi.ac.uk>
2021-03-30 15:54:37 +0100

    Removing unused (and now non-functional) email notification update. Minor updates for required fields and PEP8 compliance.

--- src/scripts/metadata_client.py
+++ src/scripts/metadata_client.py
@@ -24,42 +24,36 @@ if __name__ == '__main__':
                         choices=['submit', 'retrieve', 'list', 'delete', 'email', 'kill_job'], required=True)
     parser.add_argument('-i', '--job_id', help='Metadata job identifier to retrieve')
     parser.add_argument('-v', '--verbose', help='Verbose output', action='store_true')
-    parser.add_argument('-o', '--output_file', help='File to write output as JSON', type=argparse.FileType('w'))
+    parser.add_argument('-o', '--output_file', help='File to write output as JSON',
+                        type=argparse.FileType('w'))
     parser.add_argument('-f', '--input_file', help='File containing list of metadata and database URIs',
                         type=argparse.FileType('r'))
-    parser.add_argument('-d', '--database_uri', help='URI of database to load')
+    parser.add_argument('-d', '--database_uri', help='URI of database to load', required=True)
     parser.add_argument('-s', '--e_release', help='Ensembl release number')
     parser.add_argument('-r', '--release_date', help='Release date')
     parser.add_argument('-c', '--current_release', help='Is this the current release')
     parser.add_argument('-g', '--eg_release', help='EG release number')
-    parser.add_argument('-e', '--email', help='Email where to send the report')
-    parser.add_argument('-n', '--comment', help='Comment')
-    parser.add_argument('-b', '--source', help='Source of the database, eg: Handover, Release load')
-    parser.add_argument('-m', '--email_notification', help='Get email notification of handover progress')
+    parser.add_argument('-e', '--email', help='Submitter', required=True)
+    parser.add_argument('-n', '--comment', help='Comment', required=True)
+    parser.add_argument('-b', '--source', help='Source of the database, eg: Handover, Release load', required=True)
 
     args = parser.parse_args()
 
-    if args.verbose == True:
+    if args.verbose is True:
         logging.basicConfig(level=logging.DEBUG, format='%(message)s')
     else:
         logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-    if args.uri.endswith('/') == False:
+    if args.uri.endswith('/') is False:
         args.uri = args.uri + '/'
 
     client = MetadataClient(args.uri)
 
     if args.action == 'submit':
-        if args.input_file == None and args.email_notification == None:
+        if args.input_file is None:
             logging.info("Submitting " + args.database_uri + " for metadata load")
             job_id = client.submit_job(args.database_uri, args.e_release, args.eg_release, args.release_date,
-                                       args.current_release, args.email, args.comment, args.source, None)
-            logging.info('Job submitted with ID ' + str(job_id))
-        elif args.input_file == None:
-            logging.info("Submitting " + args.database_uri + " for metadata load")
-            job_id = client.submit_job(args.database_uri, args.e_release, args.eg_release, args.release_date,
-                                       args.current_release, args.email, args.comment, args.source,
-                                       args.email_notification)
+                                       args.current_release, args.email, args.comment, args.source)
             logging.info('Job submitted with ID ' + str(job_id))
         else:
             for line in args.input_file:
@@ -70,12 +64,10 @@ if __name__ == '__main__':
                 logging.info('Job submitted with ID ' + str(job_id))
 
     elif args.action == 'retrieve':
-
         job = client.retrieve_job(args.job_id)
         client.print_job(job, print_results=True, print_input=True)
 
     elif args.action == 'list':
-
         jobs = client.list_jobs()
         for job in jobs:
             client.print_job(job)
